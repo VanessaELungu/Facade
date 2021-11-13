8216e0c61b61563f398fa72fc4f6cc91fdbe2592
VanessaELungu <vanessa.e.lungu@accenture.com>
2021-11-13 23:26:01 +0200

    alarm set up

---
+++ Include/Alarm.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifndef alarm_h
+#define alarm_h
+
+using namespace std;
+
+class Alarm
+{
+    public:
+    void alarm_on();
+    void alarm_off();
+};
+
+#endif
---
+++ SRC/Alarm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "Alarm.h"
+
+using namespace std;
+
+void Alarm::alarm_on()
+    {
+        cout << "Alarm is on" << endl;
+    }
+
+void Alarm::alarm_off()
+    {
+        cout << "Alarm is off" << endl;
+    }
