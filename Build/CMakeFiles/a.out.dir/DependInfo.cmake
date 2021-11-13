2ea02b8b711f47a987b7d759740f73281c142853
VanessaELungu <vanessa.e.lungu@accenture.com>
2021-11-13 23:45:35 +0200

    build finall

---
+++ Build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/SRC/AC.cpp" "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/Build/CMakeFiles/a.out.dir/SRC/AC.cpp.o"
+  "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/SRC/Alarm.cpp" "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/Build/CMakeFiles/a.out.dir/SRC/Alarm.cpp.o"
+  "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/SRC/Facade.cpp" "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/Build/CMakeFiles/a.out.dir/SRC/Facade.cpp.o"
+  "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/SRC/TV.cpp" "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/Build/CMakeFiles/a.out.dir/SRC/TV.cpp.o"
+  "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/SRC/main.cpp" "/mnt/c/Users/vanessa.e.lungu/DesignPatterns/Structural/Facade/Build/CMakeFiles/a.out.dir/SRC/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/SRC/AC.cpp.o"
+  "CMakeFiles/a.out.dir/SRC/Alarm.cpp.o"
+  "CMakeFiles/a.out.dir/SRC/Facade.cpp.o"
+  "CMakeFiles/a.out.dir/SRC/TV.cpp.o"
+  "CMakeFiles/a.out.dir/SRC/main.cpp.o"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
