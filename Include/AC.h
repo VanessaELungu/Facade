4e8bd85489124d7222679b9beff5bba37bb10186
VanessaELungu <vanessa.e.lungu@accenture.com>
2021-11-13 23:29:31 +0200

    ac setup

---
+++ Include/AC.h
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#ifndef ac_h
+#define ac_h
+
+using namespace std;
+
+class AC
+{
+    public:
+    void ac_on();
+    void ac_off();
+};
+
+#endif
---
+++ SRC/AC.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "AC.h"
+
+using namespace std;
+
+void AC::ac_on()
+{
+    cout << "AC is on" << endl;
+}
+
+void AC::ac_off()
+{
+    cout << "AC is off" << endl;
+}
