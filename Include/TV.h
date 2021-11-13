b23968c7428cb8fbb11f3850e54103848bf980aa
VanessaELungu <vanessa.e.lungu@accenture.com>
2021-11-13 23:32:25 +0200

    tv set up

---
+++ Include/TV.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#ifndef tv_h
+#define tv_h
+
+using namespace std;
+
+class TV
+{
+    void tv_on();
+    void tv_off();
+};
+
+#endif
---
+++ SRC/TV.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "TV.h"
+
+using namespace std;
+
+void TV::tv_on()
+{
+    cout << "TV is on" << endl;
+}
+
+void TV::tv_off()
+{
+    cout << "TV is off" << endl;
+}
